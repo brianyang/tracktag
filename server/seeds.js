a0ff4ef48e9d8a240c7e4fabfff5405f5169b902
Brian Yang <byang@workmarket.com>
2015-09-29 21:32:48 -0400

    add track

--- server/seeds.js
+++ server/seeds.js
@@ -4,41 +4,35 @@ var files = fs.readdirSync('../../../../../server/songs/')
 console.log(id3js)
 Meteor.startup(function () {
 
-  if (Contacts.find({}).count() < 15) {
-    _(3).times(function(n) {
+  if (Contacts.find({}).count() < 55) {
+    _(1).times(function(n) {
       var user = Fake.user();
 	  var tracksArr = files
 	  var tracksObj = []
+	  console.log('foo')
 	  tracksArr.forEach(function(track){
-		  obj = {
-			  track: track,
-			  artist: 'artist'
-		  }
-		  tracksObj.push(obj)
+		  id3js({ file: '../../../../../server/songs/' + track, type: id3js.OPEN_LOCAL }, addTrack)
 	  })
-	  var track = tracksArr[0]
-	  id3js({ file: '../../../../../server/songs/' + track, type: id3js.OPEN_LOCAL }, function(err, tags) {
-		  console.log('tags', tags.title)
-		  console.log('tags', tags.artist)
-	      // tags now contains your ID3 tags
-	})
 
-      Contacts.insert({
-        name: {
-          first: tracksObj[n].track,
-          last: tracksObj[n].artist
-        },
-        emails: [{label: 'Work', address: user.email}],
-        priority: Fake.fromArray(['High', 'Medium', 'Low']),
-        location: {
-          city: Fake.word()
-        },
-        details: {
-		  tags: '',
-          notes: Fake.paragraph(),
-          active: Fake.fromArray([true, false])
-        }
-      });
+	  function addTrack(err, tags) {
+		  console.log('err, tags', err, tags)
+		  Contacts.insert({
+			name: {
+			  first: 'tracksObj[n].title',
+			  last:'tracksObj[n].artist'
+			},
+			emails: [{label: 'Work', address: user.email}],
+			priority: Fake.fromArray(['High', 'Medium', 'Low']),
+			location: {
+			  city: Fake.word()
+			},
+			details: {
+			  tags: '',
+			  notes: Fake.paragraph(),
+			  active: Fake.fromArray([true, false])
+			}
+		  });
+	  }
     });
   }
 
